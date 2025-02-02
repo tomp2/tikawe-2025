f10ea840c43456c4c0d92f3925cc37fd636b50da
tomp2 <51736857+tomp2@users.noreply.github.com>
2025-02-02 20:34:23 +0200

    feat: add initial database schema

---
+++ schema.sql
@@ -0,0 +1,66 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id            INTEGER PRIMARY KEY AUTOINCREMENT,
+    username      TEXT UNIQUE NOT NULL,
+    password_hash TEXT        NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS doodles
+(
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    title       TEXT    NOT NULL,
+    description TEXT    NOT NULL,
+    tags        TEXT    NOT NULL,
+    image_data  BLOB    NOT NULL,
+    user_id     INTEGER NOT NULL,
+    created_at  TEXT    NOT NULL,
+    views       INTEGER DEFAULT 0,
+    likes       INTEGER DEFAULT 0,
+    reactions   JSON    DEFAULT '{}',
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS comments
+(
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    doodle_id  INTEGER NOT NULL,
+    user_id    INTEGER NOT NULL,
+    content    TEXT    NOT NULL,
+    created_at TEXT    NOT NULL,
+    FOREIGN KEY (doodle_id) REFERENCES doodles (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS likes
+(
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    doodle_id  INTEGER NOT NULL,
+    user_id    INTEGER NOT NULL,
+    created_at TEXT    NOT NULL,
+    UNIQUE (doodle_id, user_id),
+    FOREIGN KEY (doodle_id) REFERENCES doodles (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS views
+(
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    doodle_id INTEGER NOT NULL,
+    user_id   INTEGER,
+    viewed_at TEXT    NOT NULL,
+    FOREIGN KEY (doodle_id) REFERENCES doodles (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS reactions
+(
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    doodle_id  INTEGER NOT NULL,
+    user_id    INTEGER NOT NULL,
+    emoji      TEXT    NOT NULL,
+    created_at TEXT    NOT NULL,
+    UNIQUE (doodle_id, user_id, emoji),
+    FOREIGN KEY (doodle_id) REFERENCES doodles (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
